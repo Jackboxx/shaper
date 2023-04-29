eb1f91c7458eba8029be6822ec2156d2636b6da1
Moritz Gschwantner <moritz.gschwantner@timetac.com>
2023-04-29 20:05:09 +0200

    Add spacing and border functions

--- examples/rect.rs
+++ examples/rect.rs
@@ -1,8 +1,14 @@
-use shaper::rect::draw_rect;
+use shaper::rect::{draw_rect, RectStyle};
 
 fn main() {
     println!(
         "{}",
-        draw_rect("hi there fellow stranger", (20, 10), (2, 1, 0, 2))
+        draw_rect(
+            "hi there fellow stranger",
+            (20, 10),
+            RectStyle::default()
+                .set_padding((2, 3, 0, 3))
+                .set_margin((3, 3, 2, 4))
+        )
     );
 }
--- src/rect.rs
+++ src/rect.rs
@@ -1,20 +1,110 @@
 use std::fmt::Display;
 
-pub fn draw_rect<D: Display>(
-    content: D,
-    size: (u16, u16),
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Default, PartialEq, Clone, Serialize, Deserialize)]
+pub struct RectStyle {
     padding: (u16, u16, u16, u16),
-) -> String {
+    margin: (u16, u16, u16, u16),
+}
+
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+pub struct BorderStyle {
+    vert_symbol: char,
+    hori_symbol: char,
+    corners: (char, char, char, char),
+}
+
+impl Default for BorderStyle {
+    fn default() -> Self {
+        Self {
+            vert_symbol: '│',
+            hori_symbol: '─',
+            corners: ('┐', '┘', '└', '┌'),
+        }
+    }
+}
+
+impl RectStyle {
+    pub fn set_padding(mut self, padding: (u16, u16, u16, u16)) -> Self {
+        self.padding = padding;
+        self
+    }
+
+    pub fn set_margin(mut self, margin: (u16, u16, u16, u16)) -> Self {
+        self.margin = margin;
+        self
+    }
+}
+
+pub fn surround_with_spacing<D: Display>(content: D, spacing: (u16, u16, u16, u16)) -> String {
+    let (t, r, b, l) = spacing;
+    let top = "\n".repeat(t.into());
+    let bottom = "\n".repeat(b.into());
+
+    let content = content
+        .to_string()
+        .split("\n")
+        .map(|line| {
+            format!(
+                "{lp}{l}{rp}",
+                l = line.trim(),
+                lp = " ".repeat(l.into()),
+                rp = " ".repeat(r.into())
+            )
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    top + &content + &bottom
+}
+
+pub fn surrond_with_border<D: Display>(content: D, style: BorderStyle) -> String {
+    let len = content
+        .to_string()
+        .split("\n")
+        .max_by(|x, y| x.len().cmp(&y.len()))
+        .unwrap_or("")
+        .len();
+
+    let BorderStyle {
+        vert_symbol,
+        hori_symbol,
+        corners,
+    } = style;
+
+    let top = format!(
+        "{l}{m}{r}",
+        l = corners.3,
+        m = hori_symbol.to_string().repeat(len),
+        r = corners.0
+    );
+
+    let bottom = format!(
+        "{l}{m}{r}",
+        l = corners.2,
+        m = hori_symbol.to_string().repeat(len),
+        r = corners.1
+    );
+
+    let content = content
+        .to_string()
+        .split("\n")
+        .map(|line| format!("{vb}{line:len$}{vb}", vb = vert_symbol))
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    top + "\n" + &content + "\n" + &bottom
+}
+
+pub fn draw_rect<D: Display>(content: D, size: (u16, u16), style: RectStyle) -> String {
     let (x, y) = size;
-    let (t_pad, r_pad, b_pad, l_pad) = padding;
+    let (t_pad, r_pad, b_pad, l_pad) = style.padding;
 
     let w = (x - (l_pad + r_pad + 2)).into();
     let h = y - (t_pad + b_pad + 2);
 
-    let pad_line = format!("│{}│\n", vec![" "; (x - 2).into()].join(""));
-    let top = format!("┌{}┐\n", vec!["─"; (x - 2).into()].join(""));
-    let bottom = format!("└{}┘", vec!["─"; (x - 2).into()].join(""));
-
+    let pad_line = format!("{}\n", " ".repeat((x - 2).into()));
     let content_padding = vec![pad_line.clone(); h.into()];
 
     let content = content
@@ -22,21 +112,14 @@ pub fn draw_rect<D: Display>(
         .chars()
         .collect::<Vec<char>>()
         .chunks(w)
-        .map(|c| {
-            format!(
-                "│{l}{c:<w$}{r}│\n",
-                l = vec![" "; l_pad.into()].join(""),
-                c = c.iter().collect::<String>(),
-                r = vec![" "; r_pad.into()].join(""),
-            )
-        })
+        .map(|c| format!("{c:<w$}\n", c = c.iter().collect::<String>(),))
         .chain(content_padding)
-        .take((h).into())
+        .take(h.into())
         .collect::<Vec<String>>()
         .join("");
 
-    top + &vec![pad_line.clone(); t_pad.into()].join("")
-        + &content
-        + &vec![pad_line; b_pad.into()].join("")
-        + &bottom
+    let rect_content = surround_with_spacing(content, style.padding);
+    let rect = surrond_with_border(rect_content, BorderStyle::default());
+
+    surround_with_spacing(rect, style.margin)
 }
